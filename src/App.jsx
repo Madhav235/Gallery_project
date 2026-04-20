6794289b5a37bb2a12b54b17f439e5dc3ac214e1
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-04-20 22:49:56 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen bg-black px-5 py-5'>
+      <button className='w-45 h-15 bg-green-500 text-white rounded-2xl font-bold text-lg hover:cursor-pointer'>Get data</button>
+    </div>
+  )
+}
+
+export default App
